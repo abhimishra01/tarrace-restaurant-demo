4d22bb135d5c6021258a5059d33216dec7578db6
Moonwalker <moonwalker@Moonwalkers-MacBook-Air.local>
2026-02-23 23:41:18 +0530

    chore: deployment updates

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/tarrace-restaurant-demo/', 
   plugins: [react(), tailwindcss()],
 })
